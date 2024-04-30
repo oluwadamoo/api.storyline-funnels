d08eecbb2779dbc1ce706d86644b44d1b01e2dd9
oluwadamoo <mrdamilolasaliu@gmail.com>
2024-04-30 16:38:02 +0100

    use typescript

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// export * from './auth.routes'
-// export * from './blog.routes'
-// export * from './category.routes'
-// export * from './comment.routes'
-// export * from './landing-page.routes'
-// export * from './users.routes'
--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-// export * from './auth.routes'
-// export * from './blog.routes'
-// export * from './category.routes'
-// export * from './comment.routes'
-// export * from './landing-page.routes'
-// export * from './users.routes'
